f6fb9845e1b38b1baa0ffa3fc3915569bc3a5c1c
Paulo Uilian <65803142+WillianBL99@users.noreply.github.com>
2022-07-12 22:07:12 -0300

    Initial commit

---
+++ database/scripts/001-create-database.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "exemple" (
+	"id" SERIAL PRIMARY KEY,
+	"name" TEXT NOT NULL UNIQUE,
+  "apiKey" TEXT
+)
---
+++ src/config/database.ts
@@ -0,0 +1,26 @@
+import pg, { ClientConfig } from "pg";
+import AppLog from "../events/AppLog.js";
+import "./setup.js";
+
+const { Client } = pg;
+const connectionString = process.env.DATABASE_URL ?? "";
+const databaseConfig: ClientConfig = { connectionString };
+
+if (process.env.MODE === "PROD") {
+  databaseConfig.ssl = {
+    rejectUnauthorized: false,
+  };
+}
+
+const client = new Client(databaseConfig);
+exec();
+export default client;
+
+async function exec() {
+  try {
+    await client.connect();
+    AppLog("Server", "Connected to database");
+  } catch (error) {
+    AppLog("Error", `Internal error while connecting to database | ${error}`);
+  }
+}
