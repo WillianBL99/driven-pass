42f65841d9c5fe57ae9040f8f00aa192e3aa799b
paulo uilian <uilianlbarros@gmail.com>
2022-07-15 02:23:32 -0300

    implements test router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import authRoute from "./authRoute.js";
 import credentialsRoute from "./credentialRoute.js";
+import testRoute from "./test.js";
 
 const router = Router();
 
 router.use(authRoute);
 router.use(credentialsRoute);
+
+//FIXME: Route for automatic tests
+router.use(testRoute);
 
 export default router;
